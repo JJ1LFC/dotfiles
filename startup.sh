8f77a71057236d97f9d81962e51180a5ee4418d9
JJ1LFC <alt@sfc.wide.ad.jp>
2020-08-14 23:15:35 +0900

    feat: test if user exists before creating user at startup

--- startup.sh
+++ startup.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 apt update
 apt full-upgrade -y
 apt install zsh curl git vim neofetch -y
 timedatectl set-timezone Asia/Tokyo
-useradd alt -m -p '' -s /usr/bin/zsh
-passwd --expire alt
-gpasswd -a alt sudo
+if [ -z $(getent passwd alt) ]; then
+    useradd alt -m -p '' -s /usr/bin/zsh
+    passwd --expire alt
+    gpasswd -a alt sudo
+    chown alt:alt /home/alt/
+fi
 sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's|[#]*PasswordAuthentication yes|PasswordAuthentication no|g' /etc/ssh/sshd_config
 systemctl restart sshd
 sudo -u alt git clone https://github.com/JJ1LFC/dotfiles.git /home/alt/dotfiles
-chown alt:alt /home/alt/
 sudo -u alt sh /home/alt/dotfiles/install.sh
