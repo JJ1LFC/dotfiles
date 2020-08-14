e43e368e38a2a2274ca35dc67b936c6acf3c2303
JJ1LFC <alt@sfc.wide.ad.jp>
2020-08-14 23:58:22 +0900

    feat: show AA on startup

--- startup.sh
+++ startup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+echo "\
+ _              _           _  _       _    _  __  _   __               _
+(_)            | |         | || |     (_)  (_)/_ || | / _|             | |
+ _  _ __   ___ | |_   __ _ | || |      _    _  | || || |_   ___      __| |  ___ __   __
+| || '_ \ / __|| __| / _` || || |     | |  | | | || ||  _| / __|    / _` | / _ \\ \ / /
+| || | | |\__ \| |_ | (_| || || | _   | |  | | | || || |  | (__  _ | (_| ||  __/ \ V /
+|_||_| |_||___/ \__| \__,_||_||_|(_)  | |  | | |_||_||_|   \___|(_) \__,_| \___|  \_/
+                                     _/ | _/ |
+                                    |__/ |__/
+"
+
 apt update
 apt full-upgrade -y
 apt install zsh curl git vim neofetch -y
 timedatectl set-timezone Asia/Tokyo
 if [ -z $(getent passwd alt) ]; then
+    echo "##########
+    ## adding user 'alt'
+    ##########"
     useradd alt -m -p '' -s /usr/bin/zsh
     passwd --expire alt
     gpasswd -a alt sudo
