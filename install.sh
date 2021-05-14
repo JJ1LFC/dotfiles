8bc2e5def68c99e3e17af55424485cf938ddd7fe
JJ1LFC <alt@sfc.wide.ad.jp>
2021-05-14 19:59:47 +0900

    fix: automatically set permissions for simlinks

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ cd /home/alt/dotfiles/dotfiles
 find . -type d | xargs -I '{}' mkdir -p "/home/alt/{}"
 find . -type f | xargs -I '{}' ln -snfv "/home/alt/dotfiles/dotfiles/{}" "/home/alt/{}"
 
+find . -type d | xargs -I '{}' chmod 707 "/home/alt/{}"
+find . -type f | xargs -I '{}' chmod 707 "/home/alt/{}"
+
 echo "Dotfiles install success"
