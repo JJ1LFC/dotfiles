ff6d7c70d9d9a521ae364e270d86749da49abff3
JJ1LFC <alt@sfc.wide.ad.jp>
2021-05-15 10:47:31 +0900

    fix: proper modes for authorized_keys

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ cd /home/alt/dotfiles/dotfiles
 find . -type d | xargs -I '{}' mkdir -p "/home/alt/{}"
 find . -type f | xargs -I '{}' ln -snfv "/home/alt/dotfiles/dotfiles/{}" "/home/alt/{}"
 
-find . -type d | xargs -I '{}' chmod 707 "/home/alt/{}"
-find . -type f | xargs -I '{}' chmod 707 "/home/alt/{}"
+find . -type d | xargs -I '{}' chmod 755 "/home/alt/{}"
+find . -type f | xargs -I '{}' chmod 755 "/home/alt/{}"
+chmod 600 /home/alt/dotfiles/dotfiles/.ssh/authorized_keys
+
 
 echo "Dotfiles install success"
